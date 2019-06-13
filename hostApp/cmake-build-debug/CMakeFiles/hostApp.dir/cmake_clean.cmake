8f9ae7e317b5fa67bf8145b8e50ae63226ae23cb
shilo <shilo.yagoda@mail.huji.ac.il>
2019-06-13 12:44:38 +0300

    add hello world files in java and cpp

---
+++ hostApp/cmake-build-debug/CMakeFiles/hostApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hostApp.dir/main.cpp.o"
+  "hostApp.pdb"
+  "hostApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hostApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
