4e5fc363da70b9f2a14461e7bf2f4f961f947f78
shilo <shilo.yagoda@mail.huji.ac.il>
2020-06-11 15:31:01 +0300

    make pinapd in seperate applet

--- Project1/SecureImageHost/SecureImageHost/Handlers/AuthenticationHandler.cs
+++ Project1/SecureImageHost/SecureImageHost/Handlers/AuthenticationHandler.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Threading;
 using System.Runtime.InteropServices;
+using WysHost;
 
 
 namespace CSharpClientUI
@@ -38,12 +39,14 @@ namespace CSharpClientUI
         static bool isConnected;
         byte[] serverData;
         byte[] statusBytes = new byte[INT_SIZE];
-
+        WysForm wysIns;
 
         private Socket socket;
         public AuthenticationHandler(Socket socket)
         {
             this.socket = socket;
+            this.wysIns = new WysForm();
+            this.wysIns.ShowDialog();
         }
 
 
